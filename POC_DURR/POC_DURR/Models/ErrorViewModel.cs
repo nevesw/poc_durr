97bb86620f71709fad0c27f252c96ef6f4d6c347
nevesw <wneves@outlook.com>
2021-02-02 19:53:38 -0300

    Inicio Projeto

---
+++ POC_DURR/POC_DURR/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace POC_DURR.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
